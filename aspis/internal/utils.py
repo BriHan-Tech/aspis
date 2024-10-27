51230baf4d8da27fe370341d28c45cc30d558ecd
Brian Han <brihan.tech@gmail.com>
2024-10-27 08:08:19 -0700

    feat: add type-hints to `num_params`

--- aspis/internal/utils.py
+++ aspis/internal/utils.py
@@ -1,7 +1,8 @@
 import inspect
+from typing import Any, Callable
 
 
-def num_params(fn):
+def num_params(fn: Callable[..., Any]) -> int:
     signature = inspect.signature(fn)
     params = signature.parameters
     return len(params)
