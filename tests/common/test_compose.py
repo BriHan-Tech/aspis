57ed2522c87c7c2d02caf6739417ced4e0007eb6
Brian Han <brihan.tech@gmail.com>
2024-11-20 07:29:59 -0800

    feat: update tests for compose

--- tests/common/test_compose.py
+++ tests/common/test_compose.py
@@ -1,14 +1,32 @@
-from pytest import fail
-
-from aspis.common import add, compose, divide, multiply
+from aspis.common import add, compose, divide, multiply, identity
 
 
-def test_basic_compose():
+def test_single_function():
+    double = lambda x: x * 2
+    composed = compose(double)
+    assert composed(2) == 4
+    assert composed(3) == 6
+
+
+def test_two_functions():
+    double = lambda x: x * 2
+    increment = lambda x: x + 1
+    composed = compose(double, increment)
+    assert composed(2) == 6
+    assert composed(3) == 8
+
+
+def test_multiple_functions():
     assert compose(add(1), multiply(2))(3) == 7
     assert compose(multiply(2), add(1))(3) == 8
     assert compose(multiply(2), add(1), divide(3))(3) == 4
 
 
-def test_compose_with_last_binary_func():
-    merge = lambda x, y: int(str(x) + str(y))
-    assert compose(multiply(2), add(1), merge)(5, 5) == (55 + 1) * 2
+def test_compose_with_multiple_args():
+    composed = compose(lambda x: x**2, lambda x, y: x + y)
+    assert composed(2, 3) == 25
+
+
+def test_identity_function():
+    composed = compose(identity)
+    assert composed(2) == 2
