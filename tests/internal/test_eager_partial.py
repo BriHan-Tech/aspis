5206f79d617277849d3b91fbceebf67d9d9d2c2f
Brian Han <brihan.tech@gmail.com>
2024-11-20 06:19:48 -0800

    feat: add `eager_partial`

---
+++ tests/internal/test_eager_partial.py
@@ -0,0 +1,43 @@
+import pytest
+
+from functools import partial
+from operator import add
+
+from aspis.internal.eager_partial import eager_partial
+
+
+def test_eager_partial_no_args():
+    assert eager_partial(add)(1, 2) == 3
+
+
+def test_eager_partial_all_args():
+    assert eager_partial(add, 1, 2) == 3
+
+
+def test_eager_partial_insufficient_args():
+    test_add = lambda x, y, z: x + y + z
+
+    res = eager_partial(test_add, 1)
+    assert res(2, 3) == 6
+
+    res = eager_partial(test_add, 1, 2)
+    assert res(3) == 6
+
+
+def test_eager_partial_excess_args():
+    assert eager_partial(add, 1, 2, 3) == 3
+
+
+def test_eager_partial_with_varargs():
+    assert eager_partial(max, 1, 2, 3, 4, 5) == 5
+
+
+def test_eager_partial_with_non_arity_error():
+    divide = lambda x, y: x / y
+
+    with pytest.raises(ZeroDivisionError):
+        eager_partial(divide, 1, 0)
+
+
+def test_eager_partial_with_c_bindings():
+    assert eager_partial(max, 1, 2, 3) == 3
