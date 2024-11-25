605392fa566c39a56e0e2eb38988c5dfcb989f63
Brian Han <brihan.tech@gmail.com>
2024-11-24 16:16:33 -0800

    feat: add more tests for `curry`

--- tests/common/test_curry.py
+++ tests/common/test_curry.py
@@ -1,9 +1,11 @@
-from aspis.common import curry
+import pytest
+
+import aspis.common as A
 
 
 def test_curry_simple_addition():
     add = lambda a, b, c: a + b + c
-    curried_add = curry(add)
+    curried_add = A.curry(add)
 
     assert curried_add(1)(2)(3) == 6
     assert curried_add(1, 2)(3) == 6
@@ -18,7 +20,7 @@ def test_curry_variadic_function():
     def add(a, *args):
         return a + sum(args)
 
-    curried_add = curry(add)
+    curried_add = A.curry(add)
 
     assert curried_add(1) == 1
     assert curried_add(1, 2) == 3
@@ -29,7 +31,7 @@ def test_curry_variadic_kwarg_function():
     def add(a, **kwargs):
         return a + sum(kwargs.values())
 
-    curried_add = curry(add)
+    curried_add = A.curry(add)
 
     assert curried_add(1) == 1
     assert curried_add(1, b=2) == 3
@@ -40,7 +42,7 @@ def test_curry_no_arguments():
     def no_args():
         return 42
 
-    curried_no_args = curry(no_args)
+    curried_no_args = A.curry(no_args)
 
     assert curried_no_args() == 42
 
@@ -49,14 +51,14 @@ def test_curry_with_default_args():
     def add(a, b, c=10):
         return a + b + c
 
-    curried_add = curry(add)
+    curried_add = A.curry(add)
     assert curried_add(1)(2) == 13
     assert curried_add(1, 2) == 13
     assert curried_add(1, 2, 5) == 8
 
 
 def test_curry_decorator_simple_addition():
-    @curry
+    @A.curry
     def add(a: str, b, c):
         return a + b + c
 
@@ -64,3 +66,14 @@ def test_curry_decorator_simple_addition():
     assert add(1, 2)(3) == 6
     assert add(1)(2, 3) == 6
     assert add(1, 2, 3) == 6
+
+
+def test_curry_should_not_handle_over_application():
+    def add(a, b):
+        return a + b
+
+    with pytest.raises(TypeError):
+        A.curry(add)(1, 2, 3)
+
+    with pytest.raises(TypeError):
+        A.curry(add)(1)(2, 3)
