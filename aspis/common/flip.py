937c1717e6e7daae25720324fc70620a7fb42a29
Brian Han <brihan.tech@gmail.com>
2024-11-24 16:16:25 -0800

    fix: function signature for `flip`

--- aspis/common/flip.py
+++ aspis/common/flip.py
@@ -1,20 +1,25 @@
-from typing import Any, Callable
+from typing import Callable, ParamSpec, TypeVar
+
+from .curry import curry
+
+P = ParamSpec("P")
+T = TypeVar("T")
 
 
-def flip(fn: Callable[..., Any]) -> Callable[..., Any]:
+def flip(fn: Callable[..., T]) -> Callable:
     """
-    Returns a new function that, when called, invokes `fn` with arguments in reverse order.
+    Returns a new function much like the supplied one, except that the first two arguments' order is reversed.
 
     Args:
         fn : Callable[..., Any]
-            The function to be called with reversed arguments.
+            The function to be called with the first two arguments reversed.
 
     Returns:
         Callable[..., Any]
-            A new function that calls `fn` with reversed arguments.
+            A new function that calls `fn` with the first two arguments reversed.
     """
 
-    def wrapper(*args: Any, **kwargs: Any) -> Callable[..., Any]:
-        return fn(*reversed(args), **kwargs)
+    def wrapper(a, b, /, *args, **kwargs) -> T:
+        return fn(b, a, *args, **kwargs)
 
-    return wrapper
+    return curry(wrapper)
--- tests/common/test_flip.py
+++ tests/common/test_flip.py
@@ -1,45 +1,62 @@
-from pytest import fail
+import pytest
 
-from aspis.common import flip
+import aspis.common as A
 
 
 def test_basic_flip():
-    f = lambda x, y: str(x) + str(y)
-    flipped_f = flip(f)
-    assert flipped_f(1, 2) == "21"
-
-    f = lambda x: str(x)
-    flipped_f = flip(f)
-    assert flipped_f(1) == "1"
+    fn = lambda x, y: str(x) + str(y)
+    flipped_fn = A.flip(fn)
+    assert flipped_fn(1, 2) == "21"
 
 
 def test_flip_decorator():
-    @flip
+    @A.flip
     def f(x, y):
         return str(x) + str(y)
 
     assert f(1, 2) == "21"
 
 
-def test_flip_too_many_args():
-    @flip
+def test_flip_curried_fn():
+    add_str = lambda a, b, c: f"{a}{b}{c}"
+    flipped_add_str = A.flip(add_str)
+    assert callable(flipped_add_str(1)(2))
+    assert flipped_add_str(1)(2)(3) == "213"
+
+    add_str = lambda a, b, *args: f"{a}{b}{sum(args)}"
+    flipped_add_str = A.flip(add_str)
+    assert flipped_add_str(1)(2, 3, 4, 5) == "2112"
+
+
+def test_flip_over_application():
+    @A.flip
     def f(x, y):
         return str(x) + str(y)
 
-    try:
+    with pytest.raises(TypeError):
         f(1, 2, 3)
-        fail()
-    except Exception:
-        pass
 
 
-def test_flip_too_few_args():
-    @flip
-    def f(x, y):
-        return str(x) + str(y)
+def test_flip_with_keyword_arguments():
+    def greet(first_name, last_name):
+        return f"Hello, {first_name} {last_name}!"
 
-    try:
-        f(1)
-        fail()
-    except Exception:
-        pass
+    with pytest.raises(TypeError):
+        A.flip(greet)("Doe", first_name="John")("John")
+
+    def greet_successfully(first_name, last_name, greeting):
+        return f"{greeting}, {first_name} {last_name}!"
+
+    assert A.flip(greet_successfully)("Doe", greeting="Hello")("John") == "Hello, John Doe!"
+
+
+def test_flip_one_argument():
+    def single_arg(x):
+        return f"Only {x}"
+
+    flipped_single_arg = A.flip(single_arg)
+
+    assert callable(flipped_single_arg(1))
+
+    with pytest.raises(TypeError):
+        flipped_single_arg(1)(2)
